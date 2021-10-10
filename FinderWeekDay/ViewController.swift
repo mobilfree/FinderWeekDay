b2db11ad73633b66abe5a7acf686323392d59071
Artem Shchegolev <mobilfree@mail.ru>
2021-10-10 15:22:58 +0300

    setup storyboard

--- FinderWeekDay/ViewController.swift
+++ FinderWeekDay/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var monthField: UITextField!
+    @IBOutlet weak var yaerField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
     }
-
-
 }
 
